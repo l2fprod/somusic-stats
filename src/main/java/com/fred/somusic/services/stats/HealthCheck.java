d7cabc0e92468daaf819cf5171c1e1c9dcdc1c39
Frederic Lavigne <fred@L2FProd.com>
2016-01-09 21:50:52 +0100

    added metrics

--- src/main/java/com/fred/somusic/services/stats/HealthCheck.java
+++ src/main/java/com/fred/somusic/services/stats/HealthCheck.java
@@ -1,14 +1,22 @@
 package com.fred.somusic.services.stats;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
 
-@RestController
-public class HealthCheck {
+import com.fred.somusic.common.BaseTask;
 
-  @RequestMapping("/services/stats/healthcheck")
-  public String healthcheck() {
-    return "OK";
+@Component
+public class HealthCheck extends BaseTask implements HealthIndicator {
+
+  @Override
+  public Health health() {    
+    try {
+      getStatsDb();
+      return Health.up().build();
+    } catch (Exception e) {
+      return Health.down(e).build();
+    }
   }
 
 }
